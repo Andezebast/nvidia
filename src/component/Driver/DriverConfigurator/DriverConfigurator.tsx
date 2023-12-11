81b1aae55a5eb5b547ebedb9fcf17b311838b30d
Andezebast <lazarev.aa5408995@gmail.com>
2023-12-11 13:58:41 +0200

    adaptive cateory page

--- src/component/Driver/DriverConfigurator/DriverConfigurator.tsx
+++ src/component/Driver/DriverConfigurator/DriverConfigurator.tsx
@@ -1,18 +1,18 @@
-import React, {FC, useState} from 'react';
-import './DriverConfigurator.scss';
+import React, { FC, useState } from "react";
+import "./DriverConfigurator.scss";
 /*-------------------------------*/
 import DriverConfiguratorList from "./DriverConfiguratorList/DriverConfiguratorList";
 import DriverConfiguratorSearch from "./DriverConfiguratorSearch/DriverConfiguratorSearch";
 /*-------------------------------*/
 const DriverConfigurator: FC = () => {
-    const [buttonSearchBoll, setButtonSearchBull] = useState<boolean>(false);
-    /*-------------------------------*/
-    return (
-        <div className='driver-page-configurator'>
-            <DriverConfiguratorList buttonSearchBoll={buttonSearchBoll}/>
-            <DriverConfiguratorSearch setButtonSearchBull={setButtonSearchBull}/>
-        </div>
-    );
+  const [buttonSearchBoll, setButtonSearchBull] = useState<boolean>(false);
+  /*------------------------------*/
+  return (
+    <div className="driver-page-configurator">
+      <DriverConfiguratorList buttonSearchBoll={buttonSearchBoll} />
+      <DriverConfiguratorSearch setButtonSearchBull={setButtonSearchBull} />
+    </div>
+  );
 };
 
 export default DriverConfigurator;
